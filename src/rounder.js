4e0c3262f10093d3daf1c81b324bef844e4985a6
Stephen Handley <stephen.handley@gmail.com>
2020-05-26 14:51:19 -0700

    forgot to add rounder

---
+++ src/rounder.js
@@ -0,0 +1,14 @@
+export default function rounder ({decimals = 0, op = Math.round} = {}) {
+  if (!(Number.isInteger(decimals) && (decimals >= 0))) {
+    throw new Error('Argument decimals must be non-negative integer');
+  }
+
+  if (!(op && (op.constructor === Function))) {
+    throw new Error('Argument op must be a function');
+  }
+
+  const multiplier = 10 ** decimals;
+  return function round (num) {
+    return op(num * multiplier) / multiplier;
+  };
+}
